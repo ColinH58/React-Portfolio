4a78e82b32f93cb23d83b79eff786582c7804778
ColinH58 <choolwerf@gmail.com>
2022-06-23 10:30:03 -0500

    created the components to start

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import './index.css';
-import { Home } from './Components/index'
+import { Home, NavBar, Blog, Resume, Projects, Contact } from './Components/index'
 
 function App() {
   return (
     <div>
       <Routes>
+        <NavBar />
         <Route path="/" element={<Home />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Resume" element={<Resume />} />
+        <Route path="/Projects" element={<Projects />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
     </div>
   );
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,1 +1,7 @@
-export { default as Home } from "./Home"
+export { default as Home } from "./Home";
+export { default as Blog } from "./Blog";
+export { default as Contact } from "./Contact";
+export { default as Navbar } from "./Navbar";
+export { default as Projects } from "./Projects";
+export { default as Resume } from "./Resume";
+export { default as ThankYou } from "./ThankYou";
