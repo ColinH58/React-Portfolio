8ec6389f470c52628ce5c553dd1c8b48540fa2be
ColinH58 <choolwerf@gmail.com>
2022-10-28 08:54:09 -0500

    added more information to portfolio items and icons

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { UilReact, UilJavaScript, UilDatabase } from '@iconscout/react-unicons';
 
 const Projects = () => {
   return (
@@ -8,6 +9,21 @@ const Projects = () => {
       <h2>Projects Deployed on Netlify</h2>
       <div className="ProjectList">
         <ul style={{ listStyleType: "none" }}>
+        <li>
+            <a
+              href="https://mystifying-euclid-1fb75d.netlify.app/"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Weather App (OpenWeatherMap.org API)
+            </a>
+            <UilReact className="Icon" />
+            <p style={{ fontSize: "1rem" }}>
+              This is my first solo project after graduating from Fullstack Academy. This weather app, made in React,
+              uses the OpenWeatherMap API and the GeoCities API to allow you to search the local weather
+              anywhere on the planet.
+            </p>
+          </li>
           <li>
             <a
               href="https://mystifying-euclid-1fb75d.netlify.app/"
@@ -16,6 +32,12 @@ const Projects = () => {
             >
               Art Collector (Harvard Art API)
             </a>
+            <UilReact className="Icon" />
+            <p style={{ fontSize: "1rem" }}>
+              A project from the Fullstack Academy Bootcamp curriculum, this is
+              a website that lets you search for historical artifacts, art, and
+              more using the Harvard API. This project was completed in React.
+            </p>
           </li>
           <li>
             <a
@@ -23,8 +45,17 @@ const Projects = () => {
               rel="noreferrer"
               target="_blank"
             >
-              Classic Snake in Vanilla JavaScript
+              Classic Snake in JavaScript
             </a>
+            <UilJavaScript className="Icon" />
+            <p style={{ fontSize: "1rem" }}>
+              Another project from the Fullstack Academy Bootcamp curriculum.
+              This is the classic Snake game made in JavaScript. Eat the red
+              square to grow while avoiding your tail and the walls. I added a
+              difficulty slider to increase the Snake speed, as well as a score
+              tracker to keep track of your highscore (for as long as the window
+              is left open).
+            </p>
           </li>
           <li>
             <a
@@ -32,8 +63,17 @@ const Projects = () => {
               rel="noreferrer"
               target="_blank"
             >
-              Guessing Game in Vanilla JavaScript
+              Guessing Game in JavaScript
             </a>
+            <UilJavaScript className="Icon" />
+            <p style={{ fontSize: "1rem" }}>
+              Another project from the Fullstack Academy Bootcamp curriculum. In
+              this game, made with JavaScript, you are given 5 chances to guess
+              a randomly generated number. I added hints that change dynamically
+              with every guess. I also added a hint button that gives you 3
+              answers, one of which is the correct answer, the other two are
+              random numbers.
+            </p>
           </li>
           <li>
             <a
@@ -41,13 +81,20 @@ const Projects = () => {
               rel="noreferrer"
               target="_blank"
             >
-              Blackjack Game in Vanilla JavaScript
+              Blackjack Game in JavaScript
             </a>
+            <UilJavaScript className="Icon" />
+            <p style={{ fontSize: "1rem" }}>
+              My very first solo project, made before my bootcamp. This is a
+              very basic and simplified version of BlackJack, made in
+              JavaScript. I added "AI" that plays by the same rules a dealer in
+              a casino would.
+            </p>
           </li>
         </ul>
         <hr />
       </div>
-      <h2>Projects Deployed on Heroku</h2>
+      <h2>Projects Deployed on Render</h2>
       <div className="ProjectList">
         <ul style={{ listStyleType: "none" }}>
           <li>
@@ -58,6 +105,18 @@ const Projects = () => {
             >
               Exotic Animals 'R Us (eCommerce Website)
             </a>
+            <UilReact className="Icon" />
+            <UilDatabase />
+            <p style={{ fontSize: "1rem" }}>
+              The final, capstone, project of the Fullstack Academy Bootcamp
+              curriculum. This project was built with React and PostreSQL. This
+              is a faux eCommerce website with a large inventory of exotic and
+              mythical animals for purchase. You are able to search and sort the
+              inventory. You are able to register an account, login and out, add
+              animals to your cart, and read about each animal. The website will
+              alert you when you try to add an animal to your cart that you have
+              already added and preserves your cart when you leave or login.
+            </p>
           </li>
           <li>
             <a
@@ -67,6 +126,14 @@ const Projects = () => {
             >
               Wizard News (Hacker News Clone)
             </a>
+            <UilJavaScript className="Icon" />
+            <UilDatabase />
+            <p style={{ fontSize: "1rem" }}>
+              Another project from the Fullstack Academy Bootcamp curriculum.
+              This is a "Hacker News" clone with hardcoded data stored in a
+              pseudo backend and displayed in a frontend with Express routes.
+              This project was built using JavaScript, Express, and Volleyball.
+            </p>
           </li>
         </ul>
         <hr />
